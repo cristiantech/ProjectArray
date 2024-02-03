4239159d8fe7a692f32f057a81a8d749f9f903a8
Patricio <wpoc.6819@gmail.com>
2024-02-03 18:27:46 -0500

    Ordenamiento por Selección

---
+++ Array.Logic/Array.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Logic
+{
+    public class ArrayNew
+    {
+        private int[] _array;
+        private int _top;
+        public ArrayNew(int num)
+        {
+            _array = new int[num];
+            N = num;
+        }
+
+        public int N { get; }
+
+        public bool IsFull => _top == N;
+        public bool IsEmpty => _top == 0;
+
+        public void Fill(int minimo, int maximo)
+        {
+            Random random = new Random();
+
+            for (int i =0; i < N; i++) 
+            {
+                _array[i] = random.Next(minimo, maximo);
+             }
+            _top = N;
+        }
+        
+        public void Sort()
+        {
+            for (int i = 0; i < _top - 1; i++)
+            {
+                for (int j = i + 1 ; j < _top; j++)
+                {
+                    if (_array[i] > _array[j])
+                    {
+                        Change(ref _array[i], ref _array[j]);
+                    }
+                }
+                    
+            }
+        }
+
+        public void Change(ref int v1, ref int v2)
+        {
+            var aux = v1;
+            v1 = v2;
+            v2 =aux;
+        }
+
+        public void SelectionSort()
+        {
+            int actual, smaller, j, temp;
+
+            for (actual = 0; actual < _top - 1; actual++)
+            {
+                smaller = actual;
+                for (j = actual + 1; j < _top; j++)
+                    if (_array[j] < _array[smaller])
+                    {
+                        smaller = j;
+                    } 
+                temp = _array[actual];
+                _array[actual] = _array[smaller];
+                _array[smaller] = temp;
+            }
+        }
+
+        public override string ToString()
+        {
+            var output = "";
+
+            if (IsEmpty) { return "Array Empty"; }
+
+            var count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (count == 10)
+                {
+                    output += "\n";
+                    count = 0;
+                }
+                output += $"{_array[i]}\t";
+                count++;
+            }
+            return output;
+        }
+    }
+}
+
+
--- Array.UX/Program.cs
+++ Array.UX/Program.cs
@@ -1,2 +1,13 @@
-﻿
+﻿using Array.Logic;
+using static System.Console;
 
+var array = new ArrayNew(25);
+array.Fill(10, 40);
+WriteLine("\t\tArreglo Inicial\n");
+WriteLine(array);
+WriteLine("\n\t\tArreglo Ordenado por Método Burbuja\n"); 
+array.Sort();
+WriteLine(array);
+WriteLine("\n\t\tArreglo Ordenado por Selección\n"); 
+array.SelectionSort();
+WriteLine(array);
